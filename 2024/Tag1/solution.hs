6e353f4e52ad1b12d8773d4741aa09ddab8589c1
MrDiver <mrdiverlp@gmail.com>
2024-12-16 10:29:52 +0100

    small changes

--- 2024/Tag1/solution.hs
+++ 2024/Tag1/solution.hs
@@ -1,4 +1,4 @@
-import Data.List (transpose, sort)
+import Data.List (sort, transpose)
 import Foreign.Marshal.Utils (fromBool)
 
 readInt :: String -> Integer
@@ -7,27 +7,26 @@ readInt = read
 sortBoth :: [[Integer]] -> [[Integer]]
 sortBoth = transpose . map sort . transpose
 
-tuplify2 [x,y] = (x,y)
-tuplify2 _ = error "Empty Tuple is not valid"
+tuple = (,) <$> head <*> head.tail
 
-listify2 (x,y) = [x,y]
+listify2 (x, y) = [x, y]
 
-distances :: [[Integer]] -> [Integer]
-distances = map (uncurry (-).tuplify2)
+unlist f (a : b : _) = f a b
+unlist f _ = error "No empty lists allowed"
 
 -- Solution to Part 1
 solution :: String -> String
-solution = show . sum . distances . sortBoth . map (map readInt . words) . lines
+solution = show . sum . map abs . unlist (zipWith (-)) . map sort . transpose . map (map readInt . words) . lines
 
 -- Everything part 2 related
 
 countOccurence :: (Num a1, Eq a2) => [a2] -> a2 -> a1
-countOccurence l x = sum $ map (fromBool . (==x)) l
+countOccurence l x = sum $ map (fromBool . (== x)) l
 
 mapToOccurencePairs :: (Num b, Eq a2) => [a2] -> [a2] -> ([a2], [b])
 mapToOccurencePairs a b = (a, map (countOccurence b) a)
 
-solution2 = show . sum . map (uncurry (*) . tuplify2) . transpose . listify2 . uncurry mapToOccurencePairs . tuplify2 . sort . transpose . map (map readInt . words) . lines
+solution2 = show . sum . map (uncurry (*) . tuple) . transpose . listify2 . uncurry mapToOccurencePairs . tuple . sort . transpose . map (map readInt . words) . lines
 
 main :: IO ()
 main = interact solution2
